7a0526d849efaf340595210846ccf42b65c4cac5
Daksh Bhatia <daksh001b@gmail.com>
2022-12-18 12:46:48 +0530

    fb fx

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'firebase_options.dart';
 
 void main() async{
 
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
