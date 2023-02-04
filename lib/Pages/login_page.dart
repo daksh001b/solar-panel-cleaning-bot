37ddca73ceef3ed50e05eaa9457cea86195bd964
sh <shiv0050@gmail.com>
2023-02-04 17:43:42 +0530

    Google and firebase auth

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class Login extends StatelessWidget{
+class Login extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: null,
-      body: SingleChildScrollView(
-        child: Column(
-        children: <Widget>[
-          Image.asset('assets/solar_panel.png'),
-          SizedBox(height: 20),
-
-        ]
+        body: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                googleSignIn();
+              }, child: const Text("Log in with Google!"),
+            )
         )
-      )
     );
   }
+
+  googleSignIn() async {
+    GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    GoogleSignInAuthentication? gAuth = await gUser?.authentication;
+    AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: gAuth?.accessToken,
+        idToken: gAuth?.idToken
+    );
+
+    UserCredential userCredential = await FirebaseAuth.instance
+        .signInWithCredential(credential);
+    print(userCredential.user?.displayName);
+  }
+
+
 }
